175e77a4ab8fb9c6c6fcead0b30b783e6eb10289
julianof03 <jlprod08@gmail.com>
2022-07-22 19:44:14 -0300

    Finished_cssBase

---
+++ src/apps/initbutton.js
@@ -0,0 +1,5 @@
+function InitButton(){
+    return{
+        
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+function App(){
+    return{
+        
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
